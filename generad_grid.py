9cf9cb63261c76a6f20ced4a81dc944ac4ba0619
adrian.jaworski@ynov.com <adrian.jaworski@ynov.com>
2025-10-15 23:53:13 +0200

    grid finish 100%

---
+++ generad_grid.py
@@ -0,0 +1,19 @@
+import display_grid
+
+def grid():
+    min_size = 5
+    max_size = 63
+    rows = int(display_grid.input_rows)
+    columns = int(display_grid.input_columns)
+
+
+    if rows < min_size or rows > max_size or columns < min_size or columns > max_size:
+        print(f"Error: Grid size must be between {min_size} and {max_size}.")
+        return []
+    return [[0 for _ in range(columns)] for _ in range(rows)]
+
+    
+for row in grid():
+
+    print(row)
+print("Grid created successfully!") if grid() else print("Failed to create grid.")
